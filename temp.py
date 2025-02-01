347f6425d509b950bac248fe948ffb07b8e0c404
Pi <pipilu89@outlook.com>
2025-02-01 21:22:41 +0800

    setup

---
+++ temp.py
@@ -0,0 +1,15 @@
+# import sys
+# print(sys.version)
+
+# import numpy as np
+# import talib
+
+# close = np.random.random(100)
+
+# output = talib.SMA(close)
+# print(output)
+
+
+import talib
+print(len(talib.get_functions()))
+# Output : 158
